732e4be1555889428d6b92004fa9713ce1df1391
Chow Loong Jin <hyperair@debian.org>
2017-10-13 01:50:45 +0800

    Complete setup.py metadata

--- setup.py
+++ setup.py
@@ -2,18 +2,26 @@
 
 from distutils.core import setup
 
+def get_long_description():
+    with open("README.md") as f:
+        return f.read()
+
 setup(
     name='flake8-expandtab',
     version='0.1',
     description='flake8 for tab junkies',
+    long_description=get_long_description(),
     author='Chow Loong Jin',
     author_email='hyperair@debian.org',
     url='https://www.github.com/hyperair/flake8-expandtab',
+    license='MIT',
     entry_points={
         'flake8.extension': [
             'expandtab = flake8_expandtab:TabExpander',
         ],
     },
+    py_modules=['flake8_expandtab'],
+    tests_require=['mock'],
     classifiers=[
         "Framework :: Flake8",
         "Intended Audience :: Developers",
