57ae94940b4e11583cfc5b506e6414ac6ab73c26
Chow Loong Jin <hyperair@debian.org>
2017-10-13 00:32:21 +0800

    Add test

---
+++ tests/test_tabfilteredfile.py
@@ -0,0 +1,50 @@
+import tempfile
+import unittest
+
+import mock
+
+import expandtab
+
+
+class TabFilteredFileTestCase(unittest.TestCase):
+    def setUp(self):
+        self.data = (
+            "\t    foo\n"
+            "\t    \tbar\n"
+        )
+
+        self.tmpfile = tempfile.NamedTemporaryFile()
+        self.tmpfile.write(self.data)
+        self.tmpfile.flush()
+
+        self.filtered_file = expandtab.TabFilteredFile(self.tmpfile.name)
+
+        self.tab_width_patcher = mock.patch(
+            "expandtab.TabExpander", tab_width=4)
+        self.tab_width_patcher.start()
+
+    def tearDown(self):
+        self.tab_width_patcher.stop()
+
+        self.filtered_file.close()
+        self.tmpfile.close()
+
+    def test_readline(self):
+        self.assertEqual(
+            self.filtered_file.readline(),
+            "        foo\n"
+        )
+
+        self.assertEqual(
+            self.filtered_file.readline(),
+            "        \tbar\n"
+        )
+
+    def test_readlines(self):
+        self.assertEqual(
+            self.filtered_file.readlines(),
+            [
+                "        foo\n",
+                "        \tbar\n"
+            ]
+        )
